c05d5a5467eb62108d249f95e730d3b32d9019dd
pitang.hilderney <hilderney.zapf@pitang.com>
2025-05-09 19:40:57 -0300

    projeto funcionando

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#2563eb',
+        'secondary': '#4f46e5',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        mono: ['Fira Code', 'monospace']
+      },
+      screens: {
+        'xs': '475px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      },
+    },
+  },
+  plugins: [],
+}
+
